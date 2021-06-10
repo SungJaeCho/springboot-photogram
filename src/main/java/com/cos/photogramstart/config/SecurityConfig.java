9859a856d91b27583bd47a110828f7033f2a02f4
sungjaecho <liadus@naver.com>
2021-06-11 08:58:47 +0900

    1.SecurityConfig 생성 2.CSFR토큰 해제(자바스크립트, postman을 통해 데이터 전송이 가능하게 하기위해)

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화
+@Configuration // Ioc
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http); super삭제시 기본 security기능 비활성화
+        http.csrf().disable(); //csrf토큰 비활성화
+        http.authorizeRequests()
+        .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated() //이주소는 인증필요
+        .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/auth/signin")
+        .defaultSuccessUrl("/")
+        ;
+    }
+}
---
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -0,0 +1,33 @@
+package com.cos.photogramstart.web;
+
+import com.cos.photogramstart.web.dto.auth.SignupDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller // 1.Ioc 2.파일을 리턴하는 컨트롤러
+public class AuthController {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    @GetMapping("/auth/signin")
+    public String signinForm(){
+        return "auth/signin";
+    }
+
+    @GetMapping("/auth/signup")
+    public String signupForm(){
+        return "auth/signup";
+    }
+
+    // 회원가입버튼 -> /auth/signup -> /auth/signin
+    @PostMapping("/auth/signup")
+    public String signup(SignupDto signupDto){
+        log.info(signupDto.toString());
+        return "auth/signin";
+    }
+}

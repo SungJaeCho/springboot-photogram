125a54d0f44ed8a8c719d08090f92a8e211c3125
SungJaeCho <liadus@naver.com>
2021-06-13 07:19:44 +0900

    Service생성

---
+++ src/main/java/com/cos/photogramstart/service/AuthService.java
@@ -0,0 +1,11 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.user.User;
+import org.springframework.stereotype.Service;
+
+@Service // 1.Ioc 2.트랜잭션관리
+public class AuthService {
+    public void 회원가입(User user){
+
+    }
+}

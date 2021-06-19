53b3cc0804cff27eb60bf4018094a5ed0a9483d1
SungJaeCho <liadus@naver.com>
2021-06-20 06:19:31 +0900

    ｕｐｄａｔｅ

--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetailsService.java
@@ -1,6 +1,7 @@
 package com.cos.photogramstart.config.auth;
 
 
+import com.cos.photogramstart.domain.user.User;
 import com.cos.photogramstart.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,9 +15,15 @@ public class PrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    // 1. 패스워드는 알아서 체크하니 신경쓸필요없음.
+    // 2. 리턴이 잘되면 자동으로 세션을 만들어줌
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("실행됨:" +username);
-        return null;
+        User userEntity = userRepository.findByUsername(username);
+        if(userEntity == null){
+            return null;
+        } else {
+            return null;
+        }
     }
 }

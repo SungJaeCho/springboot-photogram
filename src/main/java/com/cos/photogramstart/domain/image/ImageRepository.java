948373e2d83ecf1052ca54de61e40763c17e9196
sungjaecho <liadus@naver.com>
2021-07-13 14:50:12 +0900

    61. 스토리페이지 - 페이징

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.domain.image;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,6 +12,11 @@ public interface ImageRepository extends JpaRepository<Image, Integer> {
     @Query(value = "SELECT A.* FROM image A\n" +
             "INNER JOIN subscribe B\n" +
             "ON A.userId = B.toUserId\n" +
-            "WHERE B.fromUserId = :principalId", nativeQuery = true)
-    List<Image> mStory(int principalId);
+            "WHERE B.fromUserId = :principalId ORDER BY A.id DESC"
+            , countQuery = "SELECT COUNT(*) FROM image A\n" +
+            "INNER JOIN subscribe B\n" +
+            "ON A.userId = B.toUserId\n" +
+            "WHERE B.fromUserId = :principalId"
+            , nativeQuery = true)
+    Page<Image> mStory(int principalId, Pageable pageable);
 }

ad9b6ee62df16ab263f16db9170981963fcb1e97
sungjaecho <liadus@naver.com>
2021-07-17 06:26:49 +0900

    65. 좋아요 구현 중

--- src/main/java/com/cos/photogramstart/domain/image/Image.java
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -1,5 +1,6 @@
 package com.cos.photogramstart.domain.image;
 
+import com.cos.photogramstart.domain.likes.Likes;
 import com.cos.photogramstart.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import org.hibernate.annotations.ManyToAny;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
@@ -30,6 +32,8 @@ public class Image {
     private User user;
 
     // 이미지 좋아요. 개발예정
+    @OneToMany(mappedBy = "image")
+    private List<Likes> likes;
 
     // 이미지 댓글. 개발예정
 

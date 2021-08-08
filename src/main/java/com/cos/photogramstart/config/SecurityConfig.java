a44951c7168a4708b22e4de3b8133bae7d928ae6
sungjaecho <liadus@naver.com>
2021-08-08 22:05:19 +0900

    스프링 시큐리티 설정가능함 username 파라미터명 변경, 성공후 로직을 안에서 구현 등등...

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -7,7 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @EnableWebSecurity // 해당 파일로 시큐리티를 활성화
@@ -34,6 +41,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .loginPage("/auth/signin") // GET
             .loginProcessingUrl("/auth/signin") // POST -> 스프링 시큐리티가 로그인 프로세스 진행
             .defaultSuccessUrl("/")
+//            .usernameParameter("username") // Username 파라미터 명 변경가능
+//            .successHandler(new AuthenticationSuccessHandler() { //이런식으로 로그인 성공후 로직도 설정가능하다.
+//                @Override
+//                public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+//
+//                }
+//            })
             .and()
             .oauth2Login() // form 로그인도 하는데 oauth2로그인도 할거야
             .userInfoEndpoint()// oauth2로그인을 하면 최종응답을 회원정보로 바로 받을 수 있다.(인증은 시큐리티에게 맡김)
